27b6514cb17743e6d79c35bb9a60f84a65a2e211
nfepb <nicolas.planteb@gmail.com>
2022-10-11 16:45:46 +0000

    Adds link front/back-end in views.py by adding get query to get all items and display dictionary. Renders it in get_todo_list

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from .models import Item
 
 
 # Create your views here.
 def get_todo_list(request):
-    return render(request, 'todo/todo_list.html')
+    # Query to get all items in db:
+    item = Item.objects.all()
+    # Dictionnary with all our items:
+    context = {
+        'items': items
+    }
+    return render(request, 'todo/todo_list.html', context)
