4e035132749ae52f55420ce8a74bf8854c072d1a
nfepb <nicolas.planteb@gmail.com>
2022-10-14 15:16:51 +0000

    Corrects typo l.13 in test_forms.py to call on form.errors.keys()

--- todo/test_forms.py
+++ todo/test_forms.py
@@ -10,7 +10,7 @@ class TestItemForm(TestCase):
     def test_item_name_is_required(self):
         form = ItemForm({'name': ''})
         self.assertFalse(form.is_valid())
-        self.assertIn('name', form.error.keys())
+        self.assertIn('name', form.errors.keys())
         self.assertEqual(form.errors['name'][0], 'This field is required.')
 
     def test_done_field_is_not_required(self):
