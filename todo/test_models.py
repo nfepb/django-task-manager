29620ea84185209c66d0b6c13b71280835b90ac1
nfepb <nicolas.planteb@gmail.com>
2022-10-14 17:24:20 +0000

    Adds function line l.9 in TestModels class

--- todo/test_models.py
+++ todo/test_models.py
@@ -5,5 +5,7 @@ from .models import Item
 
 
 class TestModels(TestCase):
-    item = Item.objects.create(name='Test todo item')
-    self.assertFalse(item.done)
+
+    def test_done_defaults_to_false(self):
+        item = Item.objects.create(name='Test todo item')
+        self.assertFalse(item.done)
