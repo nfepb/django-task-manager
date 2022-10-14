fddf357fd76a04e8d28ba1f86a0d56eae1c8c5a1
nfepb <nicolas.planteb@gmail.com>
2022-10-14 17:19:04 +0000

    Tests model that checks if items created are by default creating with 'done' as False in test_models.py

--- todo/test_forms.py
+++ todo/test_forms.py
@@ -1,5 +1,4 @@
 from django.test import TestCase
-from .forms import ItemForm
 
 # Create your tests here.
 
--- todo/test_models.py
+++ todo/test_models.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from .models import Item
 
 # Create your tests here.
+
+
+class TestModels(TestCase):
+    item = Item.objects.create(name='Test todo item')
+    self.assertFalse(item.done)
