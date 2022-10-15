62c1c45cc1b63b635d1813b47b8500c28fba7da1
nfepb <nicolas.planteb@gmail.com>
2022-10-15 09:39:13 +0000

    Adds development var looking for DEVELOPMENT var and will look at value, otherwise set to 'False'

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -14,6 +14,8 @@ import os
 import dj_database_url
 from pathlib import Path
 
+development = os.environ.get('DEVELOPMENT', False)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
